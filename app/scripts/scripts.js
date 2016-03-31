d9989e4197e68e0b067f83d23a96a5d8126e592c
Guest Account <divine@protonmail.com>
2016-03-31 16:56:09 -0600

    first commit

---
+++ app/scripts/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  
+  var listo = [];
+  
+  
+  var Task = function(task) {
+	this.task = task;
+	this.id = 'new';
+}
+
+
+var addTask = function(task) {
+	if(task) {
+        
+        
+        
+	}
+};
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+});
